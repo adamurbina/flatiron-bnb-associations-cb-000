feaec5b9d9196ab73a40e7e04b75ac621d62dcb6
Adam Urbina <adamurbina@gmail.com>
2018-09-30 22:08:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180930213526) do
+ActiveRecord::Schema.define(version: 20180930220532) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20180930213526) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.string   "checkin"
-    t.string   "checkout"
+    t.datetime "checkin"
+    t.datetime "checkout"
     t.integer  "listing_id"
     t.integer  "guest_id"
     t.datetime "created_at", null: false
