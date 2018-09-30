6bec1d555a0cf75f7e5abe2f81800573274f00e8
Adam Urbina <adamurbina@gmail.com>
2018-09-30 22:11:44 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
-  has_many :reviews, through: :reservations, :source => guest_id
+  has_many :reviews, through: :reservations, :source => "guest_id"
   has_many :guests, through: :reservations
 end
